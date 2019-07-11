4a167c0a2492bbaddb220c4c9dc079c01f096c03
kokos <kokonozisManos@gmail.com>
2019-07-11 17:36:47 +0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Grid from '@material-ui/core/Grid';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function radioButton(){
+    return (
+        <radioButtons > 
+            
+        </radioButtons>        
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class RadioGroup extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedValue : 'Search'
+        };
+    
+        this.radioChange = this.radioChange.bind(this);
+    }
+
+    radioChange(e) {
+        this.setState({
+            selectedValue : e.currentTarget.value
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <input  type='radio'
+                        value='Search'
+                        checked= {this.selectedValue === 'Search'}
+                        onChange= {this.radioChange}/>Search City
+                <br/>
+                <input  type='radio'
+                        value='Select'
+                        checked= {this.selectedValue === 'Select'}
+                        onChange= {this.radioChange}/>Select City
+            </div>
+        );
+    }
+}
+
+
+
+class WeatherForecast extends React.Component{
+    render(){
+        return (
+            <div style={{
+                position: 'absolute', left: '50%', top: '10%',
+                transform: 'translate(-50%, -50%)'
+            }}> 
+                <h1 style={{
+                    color:'red',
+                    }}>WEATHER FORECAST</h1>
+                <RadioGroup/>
+                <Forecast/>
+            </div>
+        );
+    } 
+}
+
+
+ReactDOM.render(
+    <WeatherForecast />,
+    document.getElementById('root')
+  );
