7a6975051db81ce197cb1276903d650fdc9357e1
Koko <KokonozisManos@gmail.com>
2019-07-13 12:48:51 +0300

    weather and details

--- src/index.js
+++ src/index.js
@@ -44,7 +44,8 @@ class WeatherForecast extends React.Component{
             info : 'pending',
             select : true,
             selectedValue: '',
-            searchValue: ''
+            searchValue: '',
+            details : ''
         }; 
         
         this.loadedResponceOutput = this.loadedResponceOutput.bind(this);
@@ -78,16 +79,16 @@ class WeatherForecast extends React.Component{
     }
 
     changeSearch = (e) =>{
-        console.log(e.currentTarget);
         this.setState( {searchValue : e.currentTarget.value });
-        console.log( this.state );
-        console.log( this.state );
     }
 
     infoPendingOutpout(){
         const cities = [
             { label : 'London, uk' , value : 'London, uk'},
-            { label : 'Sitia, Crete' , value : 'Sitia, Crete'}
+            { label : 'Sitia, Crete' , value : 'Sitia, Crete'},
+            { label : 'Paris' , value : 'Paris, FR'},
+            { label : 'Lisbon' , value : 'Lisbon'},
+            { label : 'Berlin' , value : 'Berlin, GER'}
         ];
 
         let searchB = <button onClick={this.requestWeatherData} type="button" disabled={ this.state.select } >Search</button>;
@@ -169,60 +170,193 @@ class WeatherForecast extends React.Component{
         this.setState({ info:'pending' })
     }
 
-    loadedResponceOutput= () =>{
-        console.log(this.state.list);
+    getDailyinfo(data){
+        var daily = [];
 
-        if(this.state.result.cod == "404")
-            this.addNotification("Cannot find city, retry");
+        data.map((item)=>{
+            // debugger;
+
+            if(daily.length == 0)            
+                daily.push(item)
+            else{
+                const last = daily[daily.length-1].dt * 1000;
+                const next = item.dt * 1000;
+
+                // console.log("-----------------");
+                // console.log( new Date(last).toDateString() );
+                // console.log("-----------------");
+                // console.log( new Date(next).toDateString() );
                 
+                // debugger;
+                // debugger;
+                if( next - last > 1000 * 3600 * 24){
+                    daily.push(item);
+                }
+            }
+    
+        });
 
+        return daily;
+    }
+
+    viewDetails=(item,e)=>{
+        console.log(item);
+        this.addNotification("details");
+
+        this.setState({ 
+            info : 'details',
+            details : item
+        })
+    }
+
+    loadedResponceOutput= () =>{
+        
         var location = this.state.result.city.name+"  "+this.state.result.city.country;
-        var data = this.state.result.list.slice(0,50);
+        var data = this.getDailyinfo(this.state.result.list);
+        // var data = this.state.result.list;
         var eS = <span>&nbsp;&nbsp;</span> ;
+        var img ="http://openweathermap.org/img/wn/"+data[0].weather[0].icon+"@2x.png";
         return (
             <Container>
+                <ReactNotification ref={this.notificationDOMRef} />
                 <h1 className="centered" style={{color:'red'}} >WEATHER FORECAST</h1>
                 <div className="centered">
                     <h4 > {location} </h4> 
                     {eS}{eS}
                     <button type="button" onClick={this.getNewData} >Select another location</button>
                 </div>
-                <div>
+                <Col className="toLeft">
+                    <h3>Now</h3>
+                    <img src={img} alt="icon" width="100" height="100" />
+                </Col>
+                <Col className="toRight">
                     <ul >
                         {
                             data.map((item, index) => ( 
-                            <li key={index}> 
-                                Temperature:    {(item.main.temp-273.15).toFixed(2) }       C   {eS}{eS}
-                                Min:            {(item.main.temp_min-273.15).toFixed(2) }   C   {eS}{eS}
-                                Max:            {(item.main.temp_max-273.15).toFixed(2) }   C   {eS}{eS}
-                                Humidity:       {item.main.humidity}   {eS}{eS}
-                                Date:           {item.dt_txt}
-                                <br/><br/><br/>
+                            <li onClick={(e)=>{this.viewDetails({inf:item,loc: location},e)}}  key={index} > 
+                                <div className="clickable">
+                                    Date:           { new Date(item.dt * 1000).toDateString() }
+                                    {/* Temperature:    {(item.main.temp-273.15).toFixed(2) }       C   {eS}{eS} */}
+                                    <br/><br/>
+                                    Min:            {(item.main.temp_min-273.15).toFixed(2) }   &deg;C   {eS}{eS}
+                                    Max:            {(item.main.temp_max-273.15).toFixed(2) }   &deg;C  {eS}{eS}
+                                    <br/><br/>
+                                </div>
                             </li> ))
                         }
                     </ul>
+                </Col>
+            </Container> );
+    }
+
+
+//     wind: {…}
+// ​​​​
+// deg: 330.166
+// ​​​​
+// speed: 4.32
+// main: {…}
+// ​​​​
+    signalViewAll=()=>{
+        this.setState({
+            info: 'requestSend',
+        });
+    }
+
+    getItemDetailsOutput=(item)=>{
+        var location = item.loc;
+        var eS = <span>&nbsp;&nbsp;</span> ;
+        var img ="http://openweathermap.org/img/wn/"+item.inf.weather[0].icon+"@2x.png";
+
+        return (
+            <Container>
+                <ReactNotification ref={this.notificationDOMRef} />
+                <h1 className="centered" style={{color:'red'}} >WEATHER FORECAST</h1>
+                <div className="centered">
+                    <h4 > {location} details </h4> 
+                    {eS}{eS}
+                    <button onClick={this.signalViewAll} >Go Back</button>
+                    <button type="button" onClick={this.getNewData} >Select another location</button>
                 </div>
+                <Col className="toLeft">
+                    <h3>Now</h3>
+                    <img src={img} alt="icon" width="100" height="100" />
+                </Col>
+                <Col className="toRight">
+                    <div >
+                        Date:           { (item.inf.dt_txt) }
+                        <br/><br/>
+                        Temperature:    {(item.inf.main.temp-273.15).toFixed(2) }       &deg;C   {eS}{eS}
+                        <br/><br/>
+                        Min:            {(item.inf.main.temp_min-273.15).toFixed(2) }  &deg;C   {eS}{eS}
+                        Max:            {(item.inf.main.temp_max-273.15).toFixed(2) }   &deg;C   {eS}{eS}
+                        <br/><br/>
+                        Sea level:     {(item.inf.main.sea_level).toFixed(2) }  {eS}{eS}
+                        Pressure:      {(item.inf.main.pressure).toFixed(2) }  
+                        <br/><br/>
+                        Humidity:      {(item.inf.main.humidity).toFixed(2) }  {eS}{eS}
+                        Ground level:  {(item.inf.main.grnd_level).toFixed(2) }  {eS}{eS}
+                        <br/><br/>
+                        Wind degree:   {(item.inf.wind.deg).toFixed(2) }  {eS}{eS}
+                        Wind speed:    {(item.inf.wind.speed).toFixed(2) }  {eS}{eS}
+                    </div>
+                    
+                </Col>
             </Container> );
     }
 
     render(){
         var JSX;
 
-        if(this.state.info == 'pending')
-            JSX = this.infoPendingOutpout();
-        else if (this.state.info == 'requestSend'){
-            if(!this.state.loadedResponce){
-                JSX = (
-                    <Container>
-                        <h1 className="centered" style={{color:'red'}} >WEATHER FORECAST</h1>
-                        <h3 className="centered"> Loading Data </h3>
-                    </Container>    
-                    );
-            }else{
-                JSX = this.loadedResponceOutput();
-            }
+
+        switch(this.state.info){
+
+            case 'pending':
+                JSX = this.infoPendingOutpout();
+                break;
+
+            case 'requestSend':
+                
+                if(!this.state.loadedResponce){
+                    JSX = (
+                        <Container>
+                            <ReactNotification ref={this.notificationDOMRef} />
+                            <h1 className="centered" style={{color:'red'}} >WEATHER FORECAST</h1>
+                            <h3 className="centered"> Loading Data </h3>
+                        </Container>    
+                        );
+                }else{
+                    
+                    switch(this.state.result.cod){
+                        case'404':
+                            this.addNotification("Cannot find city, retry");
+                            this.setState({ info : 'pending'})
+                            JSX = this.infoPendingOutpout();
+                            break;
+                        
+                        case '200':
+                            JSX = this.loadedResponceOutput();
+                            break;
+
+                        default:
+                            this.addNotification("Unable to fetch data, retry");
+                            this.setState({ info : 'pending'})
+                            JSX = this.infoPendingOutpout();
+                            break;
+                    }
+                }
+                break;
+                
+                case'details':
+                    JSX = this.getItemDetailsOutput(this.state.details);
+                    break;
+
+            default:
+                JSX = this.infoPendingOutpout();
+                console.log('You must not be seeing this');
+                break;
         }
-        
+
         return JSX;
     } 
 }
